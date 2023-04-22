d54f3d2c665db1355db784453d53a90bef0674b1
Orest Klymko <116631168+OrestKlymko@users.noreply.github.com>
2023-04-22 09:56:30 +0200

    update Load More

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -11,26 +11,27 @@ export class ImageGallery extends Component {
     collection: [],
     loader: false,
     loadMore: false,
-    itemsPerPage: 12,
+    page: 1,
     showModal:false,
     largeImageURL:null,
-
+  itemsPerPage: 12
   };
 
   componentDidUpdate(prevProps, prevState) {
     if (
-      prevProps.onSubmit.searchQuery!==this.props.onSubmit.searchQuery||prevState.itemsPerPage!==this.state.itemsPerPage
+      prevProps.onSubmit.searchQuery!==this.props.onSubmit.searchQuery
     ) {
       const { searchQuery } = this.props.onSubmit;
+      const {itemsPerPage} = this.state;
       if (searchQuery.trim().length > 0) {
         this.setState({ loader: true, loadMore: false });
-        const URL = `https://pixabay.com/api/?q=${searchQuery}&page=1&key=34338189-e9bdbbc7a13128854f573f779&image_type=photo&orientation=horizontal&per_page=${this.state.itemsPerPage}`;
+        const URL = `https://pixabay.com/api/?q=${searchQuery}&page=1&key=34338189-e9bdbbc7a13128854f573f779&image_type=photo&orientation=horizontal&per_page=${itemsPerPage}`;
 
         fetch(URL)
           .then(r =>r.json())
-          .then(data => {
-            if (data.total > 0) {
-              this.setState({ collection: data.hits, loadMore: true });
+          .then(({totalHits, hits}) => {
+            if (totalHits > 0) {
+              this.setState({ collection: hits, loadMore: hits.length >= itemsPerPage });
             }
             else alert('I can not find image')
           })
@@ -42,9 +43,33 @@ export class ImageGallery extends Component {
       }
     }
   }
-  onLoadMore = itemsPerPage => {
-    this.setState({ itemsPerPage });
-  };
+  onLoadMore = (page) => {
+    this.setState({ page })
+    const { searchQuery } = this.props.onSubmit;
+    const {itemsPerPage} = this.state;
+    const URL = `https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=34338189-e9bdbbc7a13128854f573f779&image_type=photo&orientation=horizontal&per_page=${itemsPerPage}`;
+
+    fetch(URL)
+      .then(r =>r.json())
+      .then(({ hits }) => {
+        if (hits.length >= itemsPerPage) {
+          this.setState(prevState => ({
+            collection: [ ...prevState.collection, ...hits ],
+            loadMore: true
+          }));
+        }
+        else {
+          this.setState(prevState => ({
+            collection: [ ...prevState.collection, ...hits ],
+            loadMore: false
+          }));
+        }
+      })
+      .finally(() => {
+        this.setState({ loader: false });
+      });
+  }
+
 
   getLargeImg=(largeImageURL)=>{
     this.setState({largeImageURL})
@@ -67,8 +92,8 @@ export class ImageGallery extends Component {
         {this.state.loader && <Loader />}
         {this.state.loadMore && (
           <LoadMore
-            onClick={this.onLoadMore}
-            onCount={this.state.itemsPerPage}
+            onLoadMore={this.onLoadMore}
+            page={this.state.page}
           />
         )}
         {this.state.showModal && <Modal getLargeImg={this.state.largeImageURL} modal={this.closeModal}/>}
--- src/components/LoadMore.jsx
+++ src/components/LoadMore.jsx
@@ -2,18 +2,16 @@ import { Component } from 'react';
 import css from './css/loadMore.module.css';
 
 export class LoadMore extends Component {
-  state = {
-    itemsPerPage: 12,
-  };
 
-  onLoadMore = () => {
-    this.props.onClick(this.props.onCount + 12);
-  };
+onLoadMore =()=>{
+  const { page } =this.props
+  this.props.onLoadMore(page+1)
+}
 
   render() {
     return (
       <div className={css.btnWrapper}>
-        <button onClick={this.onLoadMore} className={css.loadMoreBtn}>
+        <button  className={css.loadMoreBtn} onClick={this.onLoadMore}>
           Load More
         </button>
       </div>
